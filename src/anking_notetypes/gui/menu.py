b7e8d8dcbc8b981ac61767789e0554dbdb112e7b
RisingOrange <jakub.fidler@protonmail.com>
2021-11-22 01:54:43 +0100

    Edit menu entry text

--- src/anking_notetypes/gui/menu.py
+++ src/anking_notetypes/gui/menu.py
@@ -5,6 +5,6 @@ from .anking_menu import get_anking_menu
 
 def setup_menu(func) -> None:
     menu = get_anking_menu()
-    a = QAction("Anking notetypes", menu)
+    a = QAction("Anking Notetypes", menu)
     menu.addAction(a)
     a.triggered.connect(lambda: func())  # type: ignore
