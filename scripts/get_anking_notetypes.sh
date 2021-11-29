c2335deb64f7d0447052df03b8cea677f6c16d2f
RisingOrange <jakub.fidler@protonmail.com>
2021-11-29 11:47:11 +0100

    Add notetypes download script

---
+++ scripts/get_anking_notetypes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Download notetype templates from AnKingMed/AnKing-Note-Types repository
+
+gitdir https://github.com/AnKingMed/AnKing-Note-Types/tree/master/Note%20Types
+
+# gitdir gets confused by %20 (urlencoded whitespace)
+rm -r Note%20Types
+
+rm -r src/anking_notetypes/note_types
+mv "Note Types" src/anking_notetypes/note_types
