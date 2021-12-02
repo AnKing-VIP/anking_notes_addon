4baa3c6e22ea69043049f7e2a7480bae014cfe53
RisingOrange <jakub.fidler@protonmail.com>
2021-12-02 16:15:28 +0100

    Update notetypes (add models)

--- scripts/get_anking_notetypes.sh
+++ scripts/get_anking_notetypes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Download notetype templates from AnKingMed/AnKing-Note-Types repository
 
-gitdir https://github.com/AnKingMed/AnKing-Note-Types/tree/master/Note%20Types
+gitdir https://github.com/RisingOrange/AnKing-Note-Types/tree/master/Note%20Types
 
 # gitdir gets confused by %20 (urlencoded whitespace)
 rm -r Note%20Types
