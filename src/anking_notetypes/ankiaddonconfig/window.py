fed2de2b5a3a1fae13dad4833a061c1054fe13cb
RisingOrange <jakub.fidler@protonmail.com>
2022-01-09 01:31:49 +0100

    Fix checkboxes not working (qt6)

--- src/anking_notetypes/ankiaddonconfig/window.py
+++ src/anking_notetypes/ankiaddonconfig/window.py
@@ -13,6 +13,8 @@ from .errors import InvalidConfigValueError
 if TYPE_CHECKING:
     from .manager import ConfigManager
 
+QT6 = QT_VERSION_STR.split(".")[0] == "6"
+
 
 class ConfigWindow(QDialog):
     def __init__(self, conf: "ConfigManager", parent=None) -> None:
@@ -183,7 +185,10 @@ class ConfigLayout(QBoxLayout):
         self.widget_updates.append(update)
 
         checkbox.stateChanged.connect(
-            lambda s: self.conf.set(key, s == Qt.CheckState.Checked)
+            lambda s: self.conf.set(
+                key,
+                s == (Qt.CheckState.Checked.value if QT6 else Qt.CheckState.Checked),
+            )
         )
         self.addWidget(checkbox)
         return checkbox
