f977c2695dc538e4bbf03dcce14c099dd36e08c9
RisingOrange <jakub.fidler@protonmail.com>
2022-07-14 11:25:38 +0200

    Change models.update_dict compat alias

--- src/anking_notetypes/compat.py
+++ src/anking_notetypes/compat.py
@@ -4,7 +4,7 @@ from anki.models import ModelManager
 def add_compat_aliases():
     add_compat_alias(ModelManager, "by_name", "byName")
     add_compat_alias(ModelManager, "add_dict", "add")
-    add_compat_alias(ModelManager, "update_dict", "update")
+    add_compat_alias(ModelManager, "update_dict", "save")
 
 
 def add_compat_alias(namespace, new_name, old_name):
