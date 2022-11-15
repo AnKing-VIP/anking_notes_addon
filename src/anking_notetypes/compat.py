500d65a58866fbd4007ed8982a71598dcb39ca9a
RisingOrange <jakub.fidler@protonmail.com>
2022-11-15 14:01:41 +0100

    fix: bug  in compatibility alias code that caues incompatibilities with some addons

--- src/anking_notetypes/compat.py
+++ src/anking_notetypes/compat.py
@@ -8,7 +8,7 @@ def add_compat_aliases():
 
 
 def add_compat_alias(namespace, new_name, old_name):
-    if new_name not in list(namespace.__dict__.keys()):
+    if new_name not in dir(namespace):
         setattr(namespace, new_name, getattr(namespace, old_name))
         return True
 
